3f84256cc431b4e07c75259f4f733e9b6d5d64e4
Bill-GD <geometrydashbill@gmail.com>
2024-05-25 13:44:46 +0700

    id for test

--- main/calculate_electricity_bill.php
+++ main/calculate_electricity_bill.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["amount"])) {
   $amount = $_POST["amount"];
   if (is_numeric($amount) && $amount >= 0) {
     $cost = getElectricityCost($amount);
-    echo "<p>The electricity cost for $amount kWh is {$cost[0]} VND</p>";
+    echo "<p id='cost_no_tax'>The electricity cost for $amount kWh is {$cost[0]} VND</p>";
     echo "<p>The cost after tax (10%) is {$cost[1]} VND</p>";
   } else {
     echo "<p>Please enter a valid number</p>";
